1abc4380ac2c233eda10dc46554eca1f34e887fd
abysspacer <artoriastheprogrammer@gmail.com>
2024-08-22 14:25:32 +0330

    create huffman prefix-code generator and fix tree-generator bug

--- huffen/__main__.py
+++ huffen/__main__.py
@@ -1,6 +1,29 @@
 from utils import get_char_freq
 import argparse
 
+def get_huffman_tree(char_freq_map):
+    freq_sorted_list = sorted([Node(char, freq) for (char, freq) in char_freq_map.items()], key=lambda x: x.freq, reverse=True) 
+    while len(freq_sorted_list) >= 2:
+        node1 = freq_sorted_list.pop()
+        node2 = freq_sorted_list.pop()
+        new_node = Node(None, node1.freq + node2.freq)
+        new_node.right = node1
+        new_node.left = node2
+        freq_sorted_list.append(new_node)
+        freq_sorted_list.sort(key=lambda x: x.freq, reverse=True)
+    if len(freq_sorted_list):
+        return freq_sorted_list[0]
+    return None
+
+def get_prefix_code_map(node, prefix_code, prefix_map):
+    if not node:
+        return
+    if node.char:
+        prefix_map[node.char] = prefix_code
+    get_prefix_code_map(node.right, prefix_code + "1", prefix_map)
+    get_prefix_code_map(node.left, prefix_code + "0", prefix_map)
+    return prefix_map
+
 class Node:
 
     right = None
@@ -21,14 +44,7 @@ args = parser.parse_args()
 
 for f in args.file_path:
     char_freq_map = get_char_freq(f)
-    freq_sorted_list = sorted([Node(char, freq) for (char, freq) in char_freq_map.items()], key=lambda x: x.freq, reverse=True) 
-    while len(freq_sorted_list) >= 2:
-        node1 = freq_sorted_list.pop()
-        node2 = freq_sorted_list.pop()
-        new_node = Node(None, node1.freq + node2.freq)
-        new_node.right = node1
-        new_node.left = node2
-        freq_sorted_list.append(new_node)
-    tree = None
-    if len(freq_sorted_list):
-        tree = freq_sorted_list[0]
+    tree = get_huffman_tree(char_freq_map)
+    prefix_code_map = get_prefix_code_map(tree, "", {})
+    for (k, v) in prefix_code_map.items():
+        print(f"{k}: {v}\n")
